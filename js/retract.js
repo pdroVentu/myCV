b7b14b1e5468a4d26474488d9244b79889be32ff
Pedro Ventura <ventura.pdro@gmail.com>
2023-09-03 20:54:51 -0300

    initial layout construction

---
+++ js/retract.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const nodesH2 = document.querySelectorAll("h2");
+    for (let node of nodesH2) {
+        node.addEventListener("dblclick", () => node.nextElementSibling.style.display = "none");
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    const nodesH2 = document.querySelectorAll("h2");
+    for (let node of nodesH2) {
+        node.addEventListener("click", () => node.nextElementSibling.style.display = "block");
+    }
+})
